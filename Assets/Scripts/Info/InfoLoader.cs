9002d0788231f66871f3bd2a56166bdeb572e7be
U-SEAKY-PC\Seaky <SeakyLuo>
2018-04-06 20:13:17 -0700

    Fixed a problem between Windows and Mac OS

--- Assets/Scripts/Info/InfoLoader.cs
+++ Assets/Scripts/Info/InfoLoader.cs
@@ -15,7 +15,7 @@ public class InfoLoader : MonoBehaviour {
 	void Start () {
         user = new CheatAccount();
         foreach(string path in Directory.GetDirectories("Assets/Resources/Board/Info"))
-            boards.Add(Resources.Load<BoardAttributes>("Board/Info/" + path.Substring(path.IndexOf("\\") + 1) + "/Attributes"));
+            boards.Add(Resources.Load<BoardAttributes>("Board/Info/" + path.Substring(path.IndexOf("Info") + 5) + "/Attributes"));
         boards = Sorted(boards);
     }
 
